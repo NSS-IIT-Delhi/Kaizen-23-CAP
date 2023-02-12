987146aa1e9d1b8e502f9d78e60e03510c84ae46
UrmilaSharma1 <ce1200264@iitd.ac.in>
2023-02-12 10:04:16 +0530

    Adding changes in CAP

--- src/components/footer/Contact.js
+++ src/components/footer/Contact.js
@@ -4,12 +4,19 @@ import img2 from './saumya.jpeg'
 import './Contact.css'
 const Contact = () => {
   return (
+<<<<<<< HEAD
     <div className="contact-background" style={{ backgroundColor: "#000", overflowX: "hidden",overflowY: "hidden", fontFamily: 'Merriweather' }}>
+=======
+    <div className="contact-background" style={{ overflowX: "hidden", fontFamily: 'Merriweather' }}>
+>>>>>>> 6f6f132 (Adding changes in CAP)
       <section id="contact" className="section-padding wow fadeIn delay-05s animated" style={{ visibility: "visible", animationName: "fadeIn" }}>
         <div className="row1">
-          <h1 className="title-text" style={{ color: " #ffa108", backgroundColor: "black" }} >
-            FOR QUERIES CONTACT US
+          <h1 className="title-text text-gradient" style={{ color: " #ffa108"}} >
+            For Queries Contact Us
           </h1>
+          <div className="hrx">
+          <hr/>
+          </div>
         </div>
         <br />
 
@@ -18,7 +25,7 @@ const Contact = () => {
             <div className="col-md-6 text-center sanmati" data-sr-id="16" style={{ visibility: "visible", opacity: 1, transform: "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)", transition: "opacity 0.6s cubic-bezier(0.5, 0, 0, 1) 0s, transform 0.6s cubic-bezier(0.5, 0, 0, 1) 0s" }}>
               <div style={{ filter: "drop-shadow(0px 0px 0px #eaebf0)" }}>
                 <img src={img1} width="250px" height="250px" />
-                <h3>HARSH KUMAR SINGH</h3>
+                <h3 className='text-gradient'>Harsh Kumar Singh</h3>
                 <h5 style={{ fontWeight: "20px" }}>CA Head</h5>
                 <h4>
                   <a href="tel:8006887753"><i className="fa fa-phone" aria-hidden="true"></i></a>
@@ -31,7 +38,7 @@ const Contact = () => {
             <div className="col-md-6 text-center aman" data-sr-id="17" style={{ visibility: "visible; opacity: 1", transform: "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)", transition: "opacity 0.6s cubic-bezier(0.5, 0, 0, 1) 0.25s, transform 0.6s cubic-bezier(0.5, 0, 0, 1) 0.25s" }}>
               <div style={{ filter: "drop-shadow(0px 0px 0px #eaebf0)" }}>
                 <img src={img2} width="250px" />
-                <h3>SOUMYA SAHU</h3>
+                <h3 className='text-gradient'>Soumya Sahu</h3>
                 <h5 style={{ fontWeight: "20px" }}>CA Head</h5>
                 <h4>
                   <a href="tel:9109405799"><i className="fa fa-phone" aria-hidden="true"></i></a>
