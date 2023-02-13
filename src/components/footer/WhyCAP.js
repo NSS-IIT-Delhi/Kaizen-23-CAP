e69b8420ed9845e3e3f91442d143f80dc7abd307
UrmilaSharma1 <ce1200264@iitd.ac.in>
2023-02-13 20:19:18 +0530

    Changes to WhyCAP5

--- src/components/footer/WhyCAP.js
+++ src/components/footer/WhyCAP.js
@@ -12,30 +12,9 @@ const WhyCAP = () => {
           <hr/>
       </div>
     <div className="container">
-      <div className="row2 row">
+      <div className="row2 ">
       <div className="col-lg-4 col-md-6 col-sm-8 col-xs-10  d-flex mt-4 mt-md-0">
-          <div className="icon-box   cap-cards">
-            <div className="icon"><i class="fa-solid fa-file-shield"></i></div>
-            <h4>Get Certificate & LOR</h4>
-            <p>Certificate of Appreciation, LOR from NSS, IIT Delhi bearing its signature.</p>
-          </div>
-        </div>
-        <div className="col-lg-4 col-md-6 col-sm-8 col-xs-10  d-flex mt-4 mt-md-0">
-          <div className="icon-box   cap-cards">
-            <div className="icon"><i class="fa-brands fa-nfc-symbol"></i></div>
-            <h4>Interconnectivity</h4>
-            <p>Better interconnectivity between IITD and the rest of the Delhi students</p>
-          </div>
-        </div>
-
-        <div className="col-lg-4 col-md-6 col-sm-8 col-xs-10  d-flex mt-4 mt-md-0">
-          <div className="icon-box   cap-cards">
-            <div className="icon"><i class="fa-solid fa-calendar-check"></i></div>
-            <h4>Organize events</h4>
-            <p>Chance to organize events and workshops in collaboration with NSS in your college.</p>
-          </div>
-        </div>
-        <div className="col-lg-4 col-md-6 col-sm-8 col-xs-10 d-flex mt-4">
+      <div className="col-lg-4 col-md-6 col-sm-8 col-xs-10 d-flex mt-4">
           <div className="icon-box   cap-cards">
             <div className="icon"><i class="fa-solid fa-star"></i></div>
             <h4>IIT Delhi events</h4>
@@ -51,6 +30,30 @@ const WhyCAP = () => {
             </p>
           </div>
         </div>
+          <div className="icon-box   cap-cards">
+            <div className="icon"><i class="fa-solid fa-file-shield"></i></div>
+            <h4>Get Certificate & LOR</h4>
+            <p>Certificate of Appreciation, LOR from NSS, IIT Delhi bearing its signature.</p>
+          </div>
+        </div>
+
+        <div className="col-lg-4 col-md-6 col-sm-8 col-xs-10  d-flex mt-4 mt-md-0">
+          <div className="icon-box   cap-cards">
+            <div className="icon"><i class="fa-brands fa-nfc-symbol"></i></div>
+            <h4>Interconnectivity</h4>
+            <p>Better interconnectivity between IITD and the rest of the Delhi students</p>
+          </div>
+        </div>
+
+        <div className="col-lg-4 col-md-6 col-sm-8 col-xs-10  d-flex mt-4 mt-md-0">
+          <div className="icon-box   cap-cards">
+            <div className="icon"><i class="fa-solid fa-calendar-check"></i></div>
+            <h4>Organize events</h4>
+            <p>Chance to organize events and workshops in collaboration with NSS in your college.</p>
+          </div>
+        </div>
+
+
 
         <div className="col-lg-4 col-md-6 col-sm-8 col-xs-10 d-flex mt-4">
           <div className="icon-box   cap-cards">
