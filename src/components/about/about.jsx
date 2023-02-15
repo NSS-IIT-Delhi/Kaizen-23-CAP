8b48dcc2eff2b0db2e172b355c7177238b3c8005
Harshal Holkar <harshalholkar813@gmail.com>
2023-02-15 12:27:28 +0530

    updates

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -8,14 +8,11 @@ const Contact = ({ id }) => {
                     <h1 className="title-text text-gradient" style={{ color: " #b6ffc5" }} >
                         About Kaizen
                     </h1>
-                    <div className="hrx" style={{width:"60%", margin : "auto"}}>
+                    <div className="hrx">
                         <hr />
                         Kaizen, IIT Delhi's annual social celebration, is thrilled to inaugurate its Campus Ambassador Program. The initiative recruits motivated students from across campus to promote the event and its goals.
-<br>
-The main objectives of Kaizen are to promote a culture of continuous improvement and encourage students to take active steps towards creating a better world. 
-<br>
-If you are passionate about making a positive impact and have a desire to grow as a leader, then this is the perfect opportunity for you.
-                        </div>
+The main objectives of Kaizen are to promote a culture of continuous improvement and encourage students to take active steps towards creating a better world. <br /> <br /> If you are passionate about making a positive impact and have a desire to grow as a leader, then this is the perfect opportunity for you.
+</div>
                 </div>
                 <br />
             </section>
