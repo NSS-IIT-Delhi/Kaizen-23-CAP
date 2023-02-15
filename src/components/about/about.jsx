1ab0728cf29ca12339b9356b517516816903302d
Harshal Holkar <93507639+Harshal-Holkar@users.noreply.github.com>
2023-02-15 11:59:22 +0530

    Update about.jsx

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -10,14 +10,12 @@ const Contact = ({ id }) => {
                     </h1>
                     <div className="hrx" style={{width:"60%", margin : "auto"}}>
                         <hr />
-                        Kaizen, IIT Delhi's social annual fest, is proud to announce the launch of its Campus Ambassador Program. The program is designed to bring together enthusiastic students from across the campus to serve as ambassadors for the event and spread awareness about its objectives and activities. As a campus ambassador, you will have the opportunity to be part of a dynamic team and gain valuable leadership and communication skills.
-<br />
-<br />
-                        The main objectives of Kaizen are to promote a culture of continuous improvement and encourage students to take active steps towards creating a better world. The event will include workshops, seminars, and competitions that will challenge participants to think creatively and come up with innovative solutions to real-world problems.
-<br />
-<br />
-                        By participating in the Campus Ambassador Program, you will not only get a chance to contribute to the success of the event but also gain valuable experience and exposure. If you are passionate about making a positive impact and have a desire to grow as a leader, then this is the perfect opportunity for you.
-                    </div>
+                        Kaizen, IIT Delhi's annual social celebration, is thrilled to inaugurate its Campus Ambassador Program. The initiative recruits motivated students from across campus to promote the event and its goals.
+<br>
+The main objectives of Kaizen are to promote a culture of continuous improvement and encourage students to take active steps towards creating a better world. 
+<br>
+If you are passionate about making a positive impact and have a desire to grow as a leader, then this is the perfect opportunity for you.
+                        </div>
                 </div>
                 <br />
             </section>
