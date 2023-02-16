25b330090d28edcc02e0b86ac3ac0e54c0c2287a
nitesh047 <niteshsaini5112001@gmail.com>
2023-02-16 14:27:20 +0530

    added analytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Home from '../src/components/Home/home'
 import KaizenForm from "./components/Kaizen Form/KaizenForm";
 import Navbar from "./components/Kaizen Form/navbar";
+import ReactGA from 'react-ga';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+const TRACKING_ID = "UA-190720982-1";
+
+ReactGA.initialize(TRACKING_ID);
 
 
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <>
       <Router>
@@ -14,7 +22,7 @@ function App() {
           <Route path="/" element={
             <Home />
           } />
-          <Route path="/form" element={<><Navbar id="home"/><KaizenForm /></>} />
+          <Route path="/form" element={<><Navbar id="home" /><KaizenForm /></>} />
         </Routes>
       </Router>
     </>
