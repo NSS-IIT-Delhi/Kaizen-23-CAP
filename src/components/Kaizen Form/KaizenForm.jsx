5479437c9b4ad4146a1c16197518c8266ac81fed
Harshal Holkar <harshalholkar813@gmail.com>
2023-02-17 15:35:46 +0530

    whatsapp link update

--- src/components/Kaizen Form/KaizenForm.jsx
+++ src/components/Kaizen Form/KaizenForm.jsx
@@ -112,10 +112,10 @@ export default class KaizenForm extends React.Component {
         }
     }
     componentDidMount() {
-        this.print_state('sts');   
+        this.print_state('sts'); 
     }
     
-    handleSubmit =  (e) => {
+    handleSubmit = (e) => {
         const form = document.getElementById('my-form');
         e.preventDefault();
         const data = new FormData(form);
@@ -3769,10 +3769,10 @@ export default class KaizenForm extends React.Component {
                                 <br />
                             </div>
                         </form>
-                                <p style={{ marginTop : "3%", textAlign : "center", color : "aquamarine"}}><a className="JoinWhchat" href="http://chat.whatsapp.com/FmbjP0IXJHZCixJ6Zc5Szm" style={{ textDecoration : "underline", color : "rgb(94, 94, 244)"}}> Join </a> for info about Kaizen events and competitions </p>
 
                     </div>
                 </div>
+                                <p style={{ marginTop : "3%", textAlign : "center", color : "aquamarine"}}><a className="JoinWhchat" href="http://chat.whatsapp.com/FmbjP0IXJHZCixJ6Zc5Szm" style={{ textDecoration : "underline", color : "rgb(94, 94, 244)"}}> Join </a> for info about Kaizen events and competitions </p>
             </section>
 
             </>
