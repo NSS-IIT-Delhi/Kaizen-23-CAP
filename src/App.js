d9b6d58a3741daecf1bc4e6eb629266bdf462466
KoRaghav <106992952+KoRaghav@users.noreply.github.com>
2023-04-12 23:44:53 +0530

    results page added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from '../src/components/Home/home'
 import KaizenForm from "./components/Kaizen Form/KaizenForm";
 import LeaderBoard from "./components/leaderboard/LeaderBoard";
 import Navbar from "./components/Kaizen Form/navbar";
+import Winners from './components/winners/LeaderBoard'
 import Team from './components/Team/Team'
 import ReactGA from 'react-ga';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -25,6 +26,7 @@ function App() {
           <Route path="/form" element={<><Navbar id="home" /><KaizenForm /></>} />
           <Route path="/team" element={<><Navbar id="home" /><Team /></>} />
           <Route path="/leaderboard" element={<><Navbar id="home" /><LeaderBoard /></>} />
+          <Route path="/winners" element={<><Navbar id="home" /><Winners /></>} />
         </Routes>
       </Router>
     </>
